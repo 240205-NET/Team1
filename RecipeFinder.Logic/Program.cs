cc096646492c4512d10dd235a4cc9ddda42408a6
M3GAalien <agamalianmike@gmail.com>
2024-02-27 16:40:25 -0500

    a lot of stuff has changed

---
+++ RecipeFinder.Logic/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using RecipeFinder.Logic.Model;
+
+namespace RecipeFinder.Logic
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Recipe Finder starting...");
+
+            var repo = new Repository();
+            Console.WriteLine(repo.ListAllUsers());
+            repo.AddNewUser();
+            Console.WriteLine("---------------------------------------------------");
+            Console.WriteLine(repo.ListAllUsers());
+        }
+    }
+}
